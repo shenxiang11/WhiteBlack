e32a39db475ebc1c33af396dad7a76d0589346d7
shenxiang <shenxiang@foursmile001.com>
2024-01-18 18:37:02 +0800

    Initial Commit

---
+++ WhiteBlock/WhiteBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteBlockApp.swift
+//  WhiteBlock
+//
+//  Created by FS on 2024/1/18.
+//
+
+import SwiftUI
+
+@main
+struct WhiteBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
